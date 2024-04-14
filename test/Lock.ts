2b967a07ea841f2ad4c19ef0bd4402527a66afbc
Eduardo <eduardojor77@gmail.com>
2024-04-14 14:12:02 -0300

    deployFixture function

--- test/Lock.ts
+++ test/Lock.ts
@@ -6,24 +6,15 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import hre from "hardhat";
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+describe("ProtoCoin", function () {
+  async function deployFixture() {
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
-    // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await hre.ethers.getSigners();
 
-    const Lock = await hre.ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const ProtoCoin = await hre.ethers.getContractFactory("ProtoCoin");
+    const protoCoin = await ProtoCoin.deploy()
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
+    return { protoCoin, owner, otherAccount };
   }
 
   describe("Deployment", function () {
