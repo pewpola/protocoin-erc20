d7d98f0200eaccc6f2861476821aa15e8a5860c0
Eduardo <eduardojor77@gmail.com>
2024-04-15 14:36:02 -0300

    created state variables

--- contracts/ProtoCoin.sol
+++ contracts/ProtoCoin.sol
@@ -2,5 +2,8 @@
 pragma solidity ^0.8.24;
 
 contract ProtoCoin {
-    
+    string public name = "ProtoCoin";
+    string public symbol = "PRC";
+    uint8 decimals = 18;
+    uint256 public totalSupply = 1000 * 10 ** decimals;
 }
