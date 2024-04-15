b86c0dda297556e206127c3efa8c1850ac997c5e
Eduardo <eduardojor77@gmail.com>
2024-04-15 15:40:56 -0300

    created Transfer and Approval events

--- contracts/ProtoCoin.sol
+++ contracts/ProtoCoin.sol
@@ -6,4 +6,7 @@ contract ProtoCoin {
     string public symbol = "PRC";
     uint8 public decimals = 18;
     uint256 public totalSupply = 1000 * 10 ** decimals;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
