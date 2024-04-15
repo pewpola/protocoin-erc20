e21189c4c8de6c3ec8c272a53c4278150d86d703
Eduardo <eduardojor77@gmail.com>
2024-04-15 14:48:33 -0300

    making decimals variable public

--- contracts/ProtoCoin.sol
+++ contracts/ProtoCoin.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.24;
 contract ProtoCoin {
     string public name = "ProtoCoin";
     string public symbol = "PRC";
-    uint8 decimals = 18;
+    uint8 public decimals = 18;
     uint256 public totalSupply = 1000 * 10 ** decimals;
 }
