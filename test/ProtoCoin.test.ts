45d31ce851ee466169412e389afd960ec1dfe3b2
Eduardo <eduardojor77@gmail.com>
2024-04-18 21:31:19 -0300

    testing transferFrom function

--- test/ProtoCoin.test.ts
+++ test/ProtoCoin.test.ts
@@ -72,4 +72,25 @@ describe("ProtoCoin", function () {
     const value = await protoCoin.allowance(owner.address, otherAccount.address);
     expect(value).to.equal(1n);
   });
+
+  it("Should transfer from", async function () {
+    const { protoCoin, owner, otherAccount } = await loadFixture(deployFixture);
+    const ownerBalanceBeforeTransfer = await protoCoin.balanceOf(owner.address);
+    const otherAccountBalanceBeforeTransfer = await protoCoin.balanceOf(otherAccount.address);
+    
+    await protoCoin.approve(otherAccount.address, 10n);
+
+    const instance = protoCoin.connect(otherAccount);
+    await instance.transferFrom(owner.address, otherAccount.address, 5n);
+
+    const ownerBalanceAfterTransfer = await protoCoin.balanceOf(owner.address);
+    const otherAccountBalanceAfterTransfer = await protoCoin.balanceOf(otherAccount.address);
+    const allowance = await protoCoin.allowance(owner.address, otherAccount.address);
+
+    expect(ownerBalanceBeforeTransfer).to.equal(1000n * 10n ** 18n);
+    expect(ownerBalanceAfterTransfer).to.equal((1000n * 10n ** 18n) - 5n);
+    expect(otherAccountBalanceBeforeTransfer).to.equal(0);
+    expect(otherAccountBalanceAfterTransfer).to.equal(5);
+    expect(allowance).to.equal(5);
+  });
 });
