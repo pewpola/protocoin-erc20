f0f742be501db7e76a2273232cadfd18308c2625
Eduardo <eduardojor77@gmail.com>
2024-04-18 21:51:52 -0300

    adding hardhat local network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,15 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    local: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk"
+      }
+    }
+  }
 };
 
 export default config;
