72fd19805583b5489981732fc6164a2bb6ae9279
Eduardo <eduardojor77@gmail.com>
2024-04-18 21:58:00 -0300

    script deploy

--- ignition/modules/Lock.ts
+++ ignition/modules/Lock.ts
@@ -1,17 +1,9 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI: bigint = 1_000_000_000n;
+const ProtoCoinModule = buildModule("ProtoCoinModule", (m) => {
+  const protoCoin = m.contract("Lock");
 
-const LockModule = buildModule("LockModule", (m) => {
-  const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
-  const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
-
-  const lock = m.contract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  return { lock };
+  return { protoCoin };
 });
 
-export default LockModule;
+export default ProtoCoinModule;
